129d989254aacbc6dfa0ddcd0912d9e0900f3777
MrZorg111 <yodji333@mail.ru>
2023-01-04 01:50:27 +0300

    Corrected version

--- Task_1.cpp
+++ Task_1.cpp
@@ -1,62 +1,55 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 int main() {
-    int num;
-    std::string fam, answer;
-    std::map<int, std::string> phone_book;
-    do {
-    std::cout << "Please fill in the phone book: \n";
-    std::cout << "Enter the number:";
-    std::cin >> num;
-    std::cout << "Enter your last name:";
-    std::cin >> fam;
+    std::string stop, surname, num;
+    std::vector<std::string> num_v;
 
-    phone_book.insert(std::pair<int, std::string> (num, fam));
-    std::cout << "As soon as you finish filling in, enter stop/go";
-    std::cin >> answer;
-    }while (answer != "stop");
-    answer = "go";
+    std::map<std::string, std::vector<std::string>> surname_num;
+    std::map<std::string, std::string> num_surname;
     do {
-        std::cout << "To find out the subscriber's last name by phone number, enter: num\n";
-        std::cout << "To find out the subscriber's number by last name, enter: fam\n";
-        std::cin >> answer;
-        if (answer == "num") {
-            int num_phone;
-            std::cout << "Enter your phone number";
-            std::cin >> num_phone;
-            for (std::map<int, std::string>::iterator it = phone_book.begin(); it != phone_book.end(); ++it) {
-                if (it->first == num_phone) {
-                    std::cout << "Subscriber's last name: " << it->second << "\n";
-                    answer = "1";
-                    break;
-                }
-            }
-            if (answer != "1") {
-                std::cout << "No subscribers with this number were found!\n";
-            }
-        } else if (answer == "fam") {
-            std::string l_name, present = "1";
-            std::cout << "Enter last name:";
-            std::cin >> l_name;
-            for (std::map<int, std::string>::iterator it = phone_book.begin(); it != phone_book.end(); ++it) {
-                if (it->second == l_name) {
-                    if (present == "1") {
-                        std::cout
-                                << "Phone numbers associated with this surname: ";
-                        present = "0";
-                    }
-                    std::cout << it->first << " ";
-                }
-            }
-            std::cout << std::endl;
+        std::cout << "Enter the subscriber's number:";
+        std::cin >> num;
+        std::cout << "Enter the subscriber's last name:";
+        std::cin >> surname;
+
+        num_surname.insert(std::pair<std::string, std::string> (num, surname));
+        if (surname_num.count(surname) > 0) {
+            std::map<std::string, std::vector<std::string>> :: iterator it = surname_num.find(surname);
+            it -> second.push_back(num);
         }
         else {
-            std::cout << "Invalid code, program stop\n";
+            num_v.resize(1);
+            num_v[0] = num;
+            surname_num.insert(std::pair<std::string, std::vector<std::string>> (surname, num_v));
         }
-        std::cout << "As soon as you finish filling in, enter stop/go ";
-        std::cin >> answer;
-    }while (answer != "stop");
+    std::cout << "To stop/continue the program, enter stop/go:";
+    std::cin >> stop;
+    }while (stop != "stop");
+    do {
+    char answer;
+    std::cout << "To search for information by last name, enter f or n to search by number:";
+    std::cin >> answer;
+    if (answer == 'n') {
+        std::cout << "Enter the phone number to search for:";
+        std::cin >> num;
+        std::map<std::string, std::string> :: iterator it = num_surname.find(num);
+        std::cout << "Subscriber's number " << it -> first << " " << it -> second << "\n";
+    }
+    else if (answer == 'f'){
+        std::cout << "Enter the subscriber's last name:";
+        std::cin >> surname;
+        std::map<std::string, std::vector<std::string>> :: iterator it = surname_num.find(surname);
+        std::cout << "Last name numbers " <<  it -> first << " ";
+        for (int i = 0; i < it -> second.size(); i++) {
+            std::cout <<  it -> second[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+        std::cout << "To stop/continue the program, enter stop/go:";
+        std::cin >> stop;
+    }while (stop != "stop");
     return 0;
 }
